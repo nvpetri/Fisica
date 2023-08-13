f73efbe2d381281aea8f233ea1842721bd541bb6
unknown <nicolaspetri18@gmail.com>
2023-08-13 19:26:51 -0300

    funcao de pesquisa implementada

--- script.js
+++ script.js
@@ -5,9 +5,70 @@ const loadingIndicator = document.getElementById('loadingIndicator')
 
 botao1.addEventListener('click', function(){
 
-    loadingIndicator.style.display = 'block';
+    loadingIndicator.style.display = 'block'
 
     setTimeout(() => {
-        window.location.href = 'index2.html';
-    }, 500); 
+        window.location.href = 'index2.html'
+    }, 500)
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("search-input")
+
+    let searchTerm = ""
+    let searchTimeout
+
+    searchInput.addEventListener("input", function() {
+        searchTerm = searchInput.value.trim()
+        clearTimeout(searchTimeout)
+
+        if (searchTerm === "") {
+            removeHighlights()
+            return;
+        }
+
+        searchTimeout = setTimeout(searchHighlight, 5000)
+    })
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            clearTimeout(searchTimeout)
+            searchHighlight()
+        }
+    })
+
+    async function searchHighlight() {
+        const resultIndex1 = window.find(searchTerm, false, false, true, false, true, false)
+        const resultIndex2 = await searchInIndex2(searchTerm)
+
+        if (!resultIndex1 && !resultIndex2) {
+            removeHighlights()
+        } else if (resultIndex2) {
+            alert("Correspondência encontrada na outra página!")
+            loadingIndicator.style.display = 'block'
+            setTimeout(() => {
+            window.location.href = 'index2.html'
+            }, 500)
+        }
+    }
+
+    function removeHighlights() {
+        const highlights = document.querySelectorAll(".highlight")
+        highlights.forEach(highlight => {
+            highlight.classList.remove("highlight")
+        })
+    }
+
+    async function searchInIndex2(term) {
+        
+        const response = await fetch("index2.html")
+        const html = await response.text()
+
+        const tempElement = document.createElement("div")
+        tempElement.innerHTML = html
+
+        return tempElement.textContent.includes(term)
+    }
+})
+
+
